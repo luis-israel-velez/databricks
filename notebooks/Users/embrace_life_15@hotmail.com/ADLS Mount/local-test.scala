c4e568664a4e177768b2e5b5dc4399ac5b5af711
Luis Israel <luis.israel.velez@gmail.com>
2019-08-17 23:59:24 -0400

    Adding into repository

---
+++ notebooks/Users/embrace_life_15@hotmail.com/ADLS Mount/local-test.scala
@@ -0,0 +1,11 @@
+//This is a test from the local machine to verify how it would work
+
+println("Hello from local machine")
+
+// COMMAND ----------
+
+val df = 1+3+5
+
+println(df)
+
+// COMMAND ----------
